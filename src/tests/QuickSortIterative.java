1b38becb7a33b6f745cf3910e80745404db2a79e
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2020-09-30 16:48:12 -0500

    QuickSortIterative added (in progress).

---
+++ src/tests/QuickSortIterative.java
@@ -0,0 +1,24 @@
+package tests;
+
+import structures.*;
+
+import java.util.List;
+
+import customStructureExceptions.*;
+
+public class QuickSortIterative {
+	
+	public int partition(List<Client> list, int low, int high) {
+		
+		double pivot = list.get(high).getAccount().getSavings();
+		
+		
+	return 0;
+	}
+	
+	public void quickSortList(List<Client> list, int low, int high) {
+		if (low < high) {
+			int middle = partition()
+		}
+	}
+}
